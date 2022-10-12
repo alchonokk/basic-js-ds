2b96f1bc28d7e9c0c0c158e019705183f42578e3
alchonokk <galunia_suglob@mail.ru>
2022-10-12 22:46:00 +0300

    feat: add solve for queue

--- src/queue.js
+++ src/queue.js
@@ -13,29 +13,47 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-let arrayofQueue= [];
+// let arrayofQueue= [];
+class Node {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
+}
 class Queue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+}
 
   getUnderlyingList() {
-    arrayofQueue.shift();
-    arrayofQueue.push(null);
-    // console.log(arrayofQueue);
-    let l=Object.assign({}, arrayofQueue);
-    // throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-    return l
+    return this.head;
   }
 
   enqueue(value) {
-    arrayofQueue.push(value);
-    // throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+    if (this.length === 0) {
+      this.head = new Node(value);
+    } else {
+      let current = this.head;
+
+      while(current.next) {
+        current = current.next;
+      }
+
+      current.next = new Node(value);
+    }
+
+    this.length++;
   }
 
   dequeue() {
-    return arrayofQueue.shift();
-    // throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const current = this.head; 
+    this.head = this.head.next; 
+    this.length--; 
+
+    return current.value;
   }
 }
 
